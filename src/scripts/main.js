5d9fd5c257af438be1a35affd9e97edddf8e5602
Павел Лашук <p.lashuk@takeoff-staff.ru>
2025-02-06 23:39:11 +0500

    add tab nav

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,19 @@
 import '../styles/styles'
-console.log("Hello from script")
+
+addEventListener("DOMContentLoaded", (event) => {
+  const toggle = document.querySelector('#menu-toggle');
+  const navigationWrapper = document.querySelector('.navigation__wrapper');
+  
+  toggle?.addEventListener('click', () => {
+    navigationWrapper?.classList.toggle('navigation__wrapper_open');
+  })
+
+  navigationWrapper?.addEventListener('click', (event) => {
+    const targetClasses = Array.from(event.target?.classList ?? []);
+
+    if (['navigation__link'].some((item) => targetClasses.includes(item))) {
+      navigationWrapper?.classList.toggle('navigation__wrapper_open');
+    }
+  })
+});
+

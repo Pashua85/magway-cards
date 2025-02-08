4ec950dbb57c1fafcf18f0bf349395ab22ab5ff4
Павел Лашук <p.lashuk@takeoff-staff.ru>
2025-02-08 22:52:43 +0500

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import autoprefixer from 'autoprefixer';
 import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 export default defineConfig({
-  base: '/magway-cards/',
+  base: '/magway-cards',
   plugins: [
     // TODO изменить цвет
     vitePluginFaviconsInject('./src/assets/favicon.svg'),

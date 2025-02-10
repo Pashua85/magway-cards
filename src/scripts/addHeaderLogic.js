68861fde724afcc439048afa3968b95277d39f5d
Павел Лашук <p.lashuk@takeoff-staff.ru>
2025-02-10 14:02:25 +0500

    update

--- src/scripts/addHeaderLogic.js
+++ src/scripts/addHeaderLogic.js
@@ -1,19 +1,31 @@
+const toggleOpenClasses = ({navigationWrapper, background}) => {
+  navigationWrapper?.classList.toggle('navigation__wrapper_open');
+  background?.classList.toggle('navigation__wrapper-background_open');
+  document.body.classList.toggle('body_stop-scrolling');
+}
+
+
 export const addHeaderLogic = () => {
   addEventListener("DOMContentLoaded", (event) => {
     const toggle = document.querySelector('#menu-toggle');
     const navigationWrapper = document.querySelector('.navigation__wrapper');
+    const background = document.querySelector('.navigation__wrapper-background');
     
     toggle?.addEventListener('click', () => {
-      navigationWrapper?.classList.toggle('navigation__wrapper_open');
+      toggleOpenClasses({ navigationWrapper, background})
     })
+
+    background?.addEventListener('click', () => {
+      console.log('click background');
+      toggleOpenClasses({ navigationWrapper, background})
+    });
   
     navigationWrapper?.addEventListener('click', (event) => {
       const targetClasses = Array.from(event.target?.classList ?? []);
   
       if (['navigation__link', 'button__icon', 'button__text', 'nav-menu-buttons__button'].some((item) => targetClasses.includes(item))) {
-        navigationWrapper?.classList.toggle('navigation__wrapper_open');
+        toggleOpenClasses({ navigationWrapper, background})
       }
     })
   });
 }
-
